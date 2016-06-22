dabbaa3c8038911ad1134a481a7e967b8d76424c
Andre Nguyen <andre-phu-van.nguyen@polymtl.ca>
2016-06-22 16:17:07 -0400

    update

--- src/rel_transform.cpp
+++ src/rel_transform.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
+
 #include <ros/ros.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <nav_msgs/Odometry.h>
@@ -12,11 +15,12 @@ ros::Publisher odom_pub;
 void callback(const geometry_msgs::PoseStampedConstPtr& pose1,
               const geometry_msgs::PoseStampedConstPtr& pose2)
 {
+  ROS_DEBUG("POSE!!!");
   geometry_msgs::TransformStamped t;
   t.header = pose1->header;
-  t.transform.translation.x = pose1->pose.position.x - pose2->pose.position.x;
+  t.transform.translation.x = -(pose1->pose.position.x - pose2->pose.position.x);
   t.transform.translation.y = pose1->pose.position.y - pose2->pose.position.y;
-  t.transform.translation.z = pose1->pose.position.z - pose2->pose.position.z;
+  t.transform.translation.z = -(pose1->pose.position.z - pose2->pose.position.z);
   tf::Quaternion qt, q1, q2;
   tf::quaternionMsgToTF(pose1->pose.orientation, q1);
   tf::quaternionMsgToTF(pose2->pose.orientation, q2);
@@ -28,6 +32,7 @@ void callback(const geometry_msgs::PoseStampedConstPtr& pose1,
 void callbackOdom(const nav_msgs::OdometryConstPtr& odom1,
                   const nav_msgs::OdometryConstPtr& odom2)
 {
+  ROS_DEBUG("ODOM!!!");
   nav_msgs::Odometry o;
   o.header = odom1->header;
   o.pose.pose.position.x = odom1->pose.pose.position.x - odom2->pose.pose.position.x;
@@ -54,29 +59,28 @@ void callbackOdom(const nav_msgs::OdometryConstPtr& odom1,
  * transform between the two poses. It assumes both messages are in the same
  * reference frame.
  */
+
+using namespace message_filters;
+
 int main(int argc, char** argv) {
   ros::init(argc, argv, "rel_transform");
   ros::NodeHandle nh;
-  message_filters::Subscriber<geometry_msgs::PoseStamped>
-      pose_sub1(nh, "pose1", 1);
-  message_filters::Subscriber<geometry_msgs::PoseStamped>
-      pose_sub2(nh, "pose2", 1);
-  message_filters::TimeSynchronizer<geometry_msgs::PoseStamped, geometry_msgs::PoseStamped>
-      sync(pose_sub1, pose_sub2, 10);
+
+  typedef sync_policies::ApproximateTime <geometry_msgs::PoseStamped, geometry_msgs::PoseStamped> PoseStampedSyncPolicy;
+  typedef sync_policies::ApproximateTime <nav_msgs::Odometry, nav_msgs::Odometry> OdomSyncPolicy;
+
+  Subscriber<geometry_msgs::PoseStamped> pose_sub1(nh, "pose1", 1);
+  Subscriber<geometry_msgs::PoseStamped> pose_sub2(nh, "pose2", 1);
+  Synchronizer<PoseStampedSyncPolicy> sync(PoseStampedSyncPolicy(10), pose_sub1, pose_sub2);
   sync.registerCallback(boost::bind(&callback, _1, _2));
 
-  message_filters::Subscriber<nav_msgs::Odometry>
-      odom_sub1(nh, "odom1", 1);
-  message_filters::Subscriber<nav_msgs::Odometry>
-      odom_sub2(nh, "odom2", 1);
-  message_filters::TimeSynchronizer<nav_msgs::Odometry, nav_msgs::Odometry>
-      sync2(odom_sub1, odom_sub2, 10);
+  Subscriber<nav_msgs::Odometry> odom_sub1(nh, "odom1", 1);
+  Subscriber<nav_msgs::Odometry> odom_sub2(nh, "odom2", 1);
+  Synchronizer<OdomSyncPolicy> sync2(OdomSyncPolicy(10), odom_sub1, odom_sub2);
   sync2.registerCallback(boost::bind(&callbackOdom, _1, _2));
 
-  transform_pub = nh.advertise<geometry_msgs::TransformStamped>
-      ("~/transform", 10);
-  odom_pub = nh.advertise<nav_msgs::Odometry>
-      ("~/odom_diff", 10);
+  transform_pub = nh.advertise<geometry_msgs::TransformStamped> ("transform", 10);
+  odom_pub = nh.advertise<nav_msgs::Odometry> ("odom_diff", 10);
 
   ros::spin();
 
